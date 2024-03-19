46a73f0903a568d5459c3fc4ef180539cd097b35
Ana St <anna1100st@gmail.com>
2024-03-19 14:22:09 -0300

    feat: new project

---
+++ oxefood-api-ana/src/main/java/br/com/ifpe/oxefood/OxefoodApiAnaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiAnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiAnaApplication.class, args);
+	}
+
+}
