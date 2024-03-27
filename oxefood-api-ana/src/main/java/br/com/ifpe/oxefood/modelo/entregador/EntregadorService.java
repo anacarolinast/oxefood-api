83f6f38efdf0cf51f6e10e74121f79a219804f7f
aluno <aluno@ifpe>
2024-03-27 20:27:04 -0300

    feat: method put

--- oxefood-api-ana/src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
+++ oxefood-api-ana/src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
@@ -30,4 +30,28 @@ public class EntregadorService {
 
         return repository.findById(id).get();
     }
+
+    @Transactional
+    public void update(Long id, Entregador entregadorAlterado) {
+
+        Entregador entregador = repository.findById(id).get();
+        entregador.setNome(entregadorAlterado.getNome());
+        entregador.setCpf(entregadorAlterado.getCpf());
+        entregador.setRg(entregadorAlterado.getRg());
+        entregador.setDataNascimento(entregadorAlterado.getDataNascimento());
+        entregador.setFoneCelular(entregadorAlterado.getFoneCelular());
+        entregador.setFoneFixo(entregadorAlterado.getFoneFixo());
+        entregador.setQtdEntregas(entregadorAlterado.getQtdEntregas());
+        entregador.setValorFrete(entregadorAlterado.getValorFrete());
+        entregador.setRua(entregadorAlterado.getRua());
+        entregador.setNumero(entregador.getNumero());
+        entregador.setBairro(entregador.getBairro());
+        entregador.setCidade(entregador.getCidade());
+        entregador.setCep(entregador.getCep());
+        entregador.setUf(entregadorAlterado.getUf());
+        entregador.setComplemento(entregadorAlterado.getComplemento());
+        entregador.setAtivo(entregadorAlterado.getAtivo());
+        entregador.setVersao(entregador.getVersao() + 1);
+        repository.save(entregador);
+    }
 }
